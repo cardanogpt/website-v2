da02fb31d43848e493a3a91b5c8e8a9e3a1769e6
MayorErnest <eobumma@gmail.com>
2024-04-13 18:29:51 +0100

    --feat:: section three

--- src/app/assets/images/index.js
+++ src/app/assets/images/index.js
@@ -1,3 +1,6 @@
 import logo from "./logo.png";
+import gridImageOne from "./gridImageOne.png";
+import gridImageTwo from "./gridImageTwo.png";
+import gridImageThree from "./gridImageThree.png";
 
-export { logo };
+export { logo, gridImageOne, gridImageTwo, gridImageThree };
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 "use client";
 
 import { useEffect } from "react";
-
+import Image from "next/image";
 import styles from "./page.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Button, Footer, Header } from "./components";
-import { DiscordIcon, SectionOneImage } from "./assets";
+import {
+	DiscordIcon,
+	SectionOneImage,
+	gridImageOne,
+	gridImageThree,
+	gridImageTwo,
+} from "./assets";
 
 export default function Home() {
 	// initialize animation
@@ -49,6 +55,107 @@ export default function Home() {
 							way for a future where technology and human
 							creativity to flourish together.
 						</p>
+					</div>
+				</section>
+				<section className={styles.sectionThree}>
+					<span className={styles.headerPills}>our solutions</span>
+					<h2 className={styles.headerThree}>
+						unlock unprecedented possibilities with cardanoGPT
+					</h2>
+					<div className={styles.sectionThreeGrid}>
+						<div>
+							<div>
+								<h4 className={styles.headerFour}>
+									General Knowledge Discord ChatBot
+								</h4>
+								<p className={styles.paragraph}>
+									CardanoGPT, inspired by Girolamo Cardano, is
+									a beta-phase AI chatbot providing real-time
+									answers and offering content generation,
+									image interpretation, voice recognition and
+									more. Exclusive access for CGI token holders
+									(meeting threshold).
+								</p>
+								<a href="#" className="anchor-link">
+									<Button variant="A">
+										Join Girolamo Beta Testers
+									</Button>
+								</a>
+							</div>
+							<div>
+								<Image
+									src={gridImageOne}
+									width={156.33}
+									height={235}
+									alt="grid-image-one"
+									placeholder="blur"
+								/>
+							</div>
+						</div>
+						<div>
+							<div>
+								<h4 className={styles.headerFour}>
+									Custom Bot Development
+								</h4>
+								<p className={styles.paragraph}>
+									CardanoGPT builds smart chatbots for
+									businesses. These chatbots can handle tasks
+									like social media management, customer
+									service, and data analysis.
+								</p>
+								<a href="#" className="anchor-link">
+									<Button variant="A">Learn more</Button>
+								</a>
+							</div>
+							<div>
+								<Image
+									src={gridImageTwo}
+									width={171}
+									height={310}
+									alt="grid-image-two"
+									placeholder="blur"
+								/>
+							</div>
+						</div>
+						<div>
+							<div>
+								<h4 className={styles.headerFour}>X Chatbot</h4>
+								<p className={styles.paragraph}>
+									AskGirolamo is your one-stop shop for
+									Cardano info on X. Get instant answers and
+									stay updated on the Cardano ecosystem, all
+									within X.
+								</p>
+								<a href="#" className="anchor-link">
+									<Button variant="A">Learn more</Button>
+								</a>
+							</div>
+							<div>
+								<Image
+									src={gridImageThree}
+									width={161}
+									height={235}
+									alt="grid-image-three"
+									placeholder="blur"
+								/>
+							</div>
+						</div>
+						<div>
+							<div>
+								<h4 className={styles.headerFour}>
+									AI Powered Writing Platform
+								</h4>
+								<p className={styles.paragraph}>
+									CardanoGPT redefines content creation. Our
+									AI writing platform (first on blockchain!)
+									empowers bloggers, marketers, and businesses
+									to craft high-impact content.
+								</p>
+								<a href="#" className="anchor-link">
+									<Button variant="A">Learn more</Button>
+								</a>
+							</div>
+						</div>
 					</div>
 				</section>
 			</main>
