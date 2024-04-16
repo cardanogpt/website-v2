37fb6f43441a9c617fb232ad3c26ba171dbe2765
MayorErnest <eobumma@gmail.com>
2024-04-16 18:42:41 +0100

    --feat:: add partners

--- src/app/assets/images/index.js
+++ src/app/assets/images/index.js
@@ -2,5 +2,20 @@ import logo from "./logo.png";
 import gridImageOne from "./gridImageOne.png";
 import gridImageTwo from "./gridImageTwo.png";
 import gridImageThree from "./gridImageThree.png";
+import bitcoinist from "./bitcoinist.png";
+import businessInsider from "./businessInsider.png";
+import cryptoslate from "./cryptoslate.png";
+import thedailyhodl from "./thedailyhodl.png";
+import utoday from "./utoday.png";
 
-export { logo, gridImageOne, gridImageTwo, gridImageThree };
+export {
+	logo,
+	gridImageOne,
+	gridImageTwo,
+	gridImageThree,
+	bitcoinist,
+	businessInsider,
+	cryptoslate,
+	thedailyhodl,
+	utoday,
+};
